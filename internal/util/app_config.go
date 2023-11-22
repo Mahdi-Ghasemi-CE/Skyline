526b38254afc1699e96eac1b965a1d5a81fa8690
Mahdi-Ghasemi-CE <mahdighasemi.shahed@gmail.com>
2023-11-22 16:42:31 +0330

    Add - app_config go & env files - for handling the app configurations from file and using it into the app

---
+++ internal/util/app_config.go
@@ -0,0 +1,24 @@
+package util
+
+import "github.com/spf13/viper"
+
+// Config stores all configuration of the application.
+// The values are read by viper from a config file or environment variable.
+type appConfig struct {
+	Environment string `mapstructure:"ENVIRONMENT"`
+}
+
+// LoadAppConfig reads configuration from file or environment variables.
+func LoadAppConfig(path string) (appConfig appConfig, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&appConfig)
+	return appConfig, err
+}
