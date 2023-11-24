0f70ecada5a11aeed620352a2fff35e989922464
Mahdi-Ghasemi-CE <mahdighasemi.shahed@gmail.com>
2023-11-24 14:04:57 +0330

    Edit - some packages structures

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"Skyline/internal/database"
 	"Skyline/internal/utils"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Hello World ")
-	appSetting, err := utils.LoadAppConfig("./configs")
+	appSetting, err := utils.LoadAppConfig("./internal/configs")
 	if err != nil {
 		panic("AppSetting cannot be loaded")
 	}
 
-	err = database.InitDB(appSetting.DbConnection)
+	err = utils.InitDB(appSetting.DbConnection)
 	if err != nil {
 		panic("Cannot connected to the database!")
 	}
