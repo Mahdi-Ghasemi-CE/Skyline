2fba13a2110bf19e0f74d2f8a34cc487480885d2
Mahdi-Ghasemi-CE <mahdighasemi.shahed@gmail.com>
2023-12-12 19:20:19 +0330

    Add - adding new items for sending email

--- internal/utils/app_config.go
+++ internal/utils/app_config.go
@@ -7,11 +7,20 @@ import (
 // Config stores all configuration of the application.
 // The values are read by viper from a config file or environment variable.
 type appConfig struct {
-	Environment       string `mapstructure:"ENVIRONMENT"`
-	DbConnection      string `mapstructure:"DB_CONNECTION"`
-	HTTPServerAddress string `mapstructure:"HTTPServerAddress"`
-	ContextTimeout    int    `mapstructure:"CONTEXT_TIMEOUT"`
+	Environment          string `mapstructure:"ENVIRONMENT"`
+	DbConnection         string `mapstructure:"DB_CONNECTION"`
+	HTTPServerAddress    string `mapstructure:"HTTPServerAddress"`
+	ContextTimeout       int    `mapstructure:"CONTEXT_TIMEOUT"`
+	AccessTokenDuration  int    `mapstructure:"ACCESS_TOKEN_DURATION"`
+	RefreshTokenDuration int    `mapstructure:"REFRESH_TOKEN_DURATION"`
+	AccessTokenSecret    string `mapstructure:"ACCESS_TOKEN_SECRET"`
+	RefreshTokenSecret   string `mapstructure:"REFRESH_TOKEN_SECRET"`
+	EmailSenderName      string `mapstructure:"EMAIL_SENDER_NAME"`
+	EmailSenderAddress   string `mapstructure:"EMAIL_SENDER_ADDRESS"`
+	EmailSenderPassword  string `mapstructure:"EMAIL_SENDER_PASSWORD"`
 }
+
+var AppConfig *appConfig
 
 // LoadAppConfig reads configuration from file or environment variables.
 func LoadAppConfig(path string) (appConfig appConfig, err error) {
@@ -24,6 +33,7 @@ func LoadAppConfig(path string) (appConfig appConfig, err error) {
 	if err != nil {
 		return
 	}
+	err = viper.Unmarshal(&AppConfig)
 	err = viper.Unmarshal(&appConfig)
 	return appConfig, err
 }
